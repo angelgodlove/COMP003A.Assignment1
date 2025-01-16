fca5f2fc97bc428bbc0d645a1d48c7c85cb6a0ab
AngelGodlove <godlove.aj@gmail.com>
2025-01-15 21:00:38 -0800

    Add project files.

---
+++ COMP003A.Assignmnet1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003A.Assignmnet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, Enter your name: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Hi " + username + ". My name is Angel.");
+            Console.WriteLine("\n Here are 10 things about me.");
+            Console.WriteLine("I am a Computer Science student at Lemoore College.");
+            Console.WriteLine("I like to be outdoors, such as fishing, spending time and walking and jogging my dog. And i love playing video games");
+            Console.WriteLine("I love all types of music and movies");
+            Console.WriteLine("I drive a Toyota Corolla";
+              
+             
+   
+        }
+    }
+}
