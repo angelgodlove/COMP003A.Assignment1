20ab274092e74c978f0eac11e4a2110fd776dacc
angelgodlove <godlove.aj@gmail.com>
2025-01-16 18:06:15 -0800

    Program.cs

--- COMP003A.Assignmnet1/Program.cs
+++ COMP003A.Assignmnet1/Program.cs
@@ -7,12 +7,18 @@
             Console.Write("Hello, Enter your name: ");
             string username = Console.ReadLine();
 
-            Console.WriteLine("Hi " + username + ". My name is Angel.");
-            Console.WriteLine("\n Here are 10 things about me.");
-            Console.WriteLine("I am a Computer Science student at Lemoore College.");
-            Console.WriteLine("I like to be outdoors, such as fishing, spending time and walking and jogging my dog. And i love playing video games");
-            Console.WriteLine("I love all types of music and movies");
-            Console.WriteLine("I drive a Toyota Corolla";
+          Console.WriteLine("Hi " + username + ". My name is Angel.");
+Console.WriteLine("\n Here are 10 things about me.");
+Console.WriteLine("I am a Computer Science student at Lemoore College.");
+Console.WriteLine("I like to be outdoors, such as fishing, spending time and walking and jogging my dog. And i love playing video games");
+Console.WriteLine("I love all types of music and movies");
+Console.WriteLine("I drive a Toyota Corolla");
+Console.WriteLine("I am the only child");
+Console.WriteLine("I have been living in Lemoore since I was 9 years old.");
+Console.WriteLine("I love to do anything that keeps me on my feet.");
+Console.WriteLine("I like Marvel Movies than DC movies");
+Console.WriteLine("I love to spend time with my family, most important my grandparents");
+Console.WriteLine("I love to travel and explore the world");
               
              
    
